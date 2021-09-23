95d906030bf62bbb422123f44f8ba0f1487950a6
Keval <bhogayatakb@gmail.com>
2021-09-23 13:36:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Welcome to App js");
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//Launch listening server on port 8081
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+})
