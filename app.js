4904f246a36b20e7f857e178c6d57f00e7d94624
Keval <bhogayatakb@gmail.com>
2021-10-08 13:13:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var app = express()
-var datetime = new Date();
+//var express = require('express')
+//var app = express()
+//var datetime = new Date();
 
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello Main World !!! ' + datetime)
-})
+//app.get('/', function (req, res) {
+//  res.send('Hello Main World !!! ' + datetime)
+//})
 
 //Launch listening server on port 8081
-app.listen(8081, function () {
+//app.listen(8081, function () {
   console.log('app listening on port 8081!')
-})
+//})
 
-app.listen(8082, function () {
-  console.log('welcome to port 8082!')
-})
+//app.listen(8082, function () {
+//  console.log('welcome to port 8082!')
+//})
