08d9e9ce14526ed71f24ab00b549dfe01adc2514
Keval <bhogayatakb@gmail.com>
2022-01-10 11:52:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var app = express()
 var datetime = new Date();
 
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
+app.use('/', function (req, res) {
   res.send('Hello Main World --3-- !!! ' + datetime)
 })
 
+console.log("1111", process.argv[3])
 var port = process.argv[3];
 //Launch listening server on port .....
 app.listen(port, function () {
